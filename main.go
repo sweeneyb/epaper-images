928454c73d6ede8e742184b483a32bfbea4acd81
sweeneyb <tab126@gmail.com>
2023-12-23 11:54:59 -0500

    add functions to serve xbm byte arrays

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
 	"net/http"
 	"os"
 
@@ -26,11 +30,11 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func main() {
-	fmt.Println("Hello, World!")
-
-	newWidth, newHeight := 840, 480
+func createImages() {
+	newWidth, newHeight := 800, 480
+	// newWidth, newHeight := 210, 120
 	blackImage := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+	redImage := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
 	// blackImage := image.NewPaletted(image.Rect(0, 0, newWidth, newHeight), color.Palette{
 	// 	color.RGBA{255, 0, 0, 255},     // Red
 	// 	color.RGBA{0, 255, 0, 255},     // Green
@@ -52,31 +56,171 @@ func main() {
 	if err := dc.LoadFontFace(fontPath, 96); err != nil {
 		panic(err)
 	}
-	dc.DrawStringAnchored("Hello, world!", float64(newWidth/2), float64(newHeight/2), 0.5, 0.5)
+	dc.DrawStringAnchored("On Air!!", float64(newWidth/2), float64(newHeight/2), 0.5, 0.5)
 
 	dc.SetRGB(0, 0, 0)
-	dc.DrawRoundedRectangle(0, 0, 20, float64(newWidth), 0)
-	dc.Fill()
+	// left
+	// dc.DrawRectangle(0, 0, 20, float64(newHeight))
+	// dc.Fill()
+	// //top
+	// dc.DrawRectangle(0, 0, float64(newWidth), 20)
+	// dc.Fill()
+	// //right
+	// dc.DrawRectangle(float64(newWidth-20), 0, 20, float64(newHeight))
+	// dc.Fill()
+	// //bottom
+	// dc.DrawRectangle(0, float64(newHeight-20), float64(newWidth), 20)
+	// dc.Fill()
 	dc.DrawImage(blackImage, 0, 0)
 	dc.Clip()
-	image := dc.Image()
+	resultImage := dc.Image()
 
-	outputFile, err := os.Create("output/go-black.bmp")
+	outputFile, err := os.Create("output/go-black3.bmp")
 	if err != nil {
 		fmt.Println("Error creating output file:", err)
 		return
 	}
 	defer outputFile.Close()
-	err = bmp.Encode(outputFile, image)
+	err = bmp.Encode(outputFile, resultImage)
 	if err != nil {
 		fmt.Println("Error encoding output image:", err)
 		return
 	}
 
+	outputFile, err = os.Create("output/go-black3.png")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	err = png.Encode(outputFile, resultImage)
+	if err != nil {
+		fmt.Println("Error encoding output image:", err)
+		return
+	}
+
+	dc = gg.NewContextForImage(redImage)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	// left
+	dc.DrawRectangle(0, 0, 20, float64(newHeight))
+	dc.Fill()
+	//top
+	dc.DrawRectangle(0, 0, float64(newWidth), 20)
+	dc.Fill()
+	//right
+	dc.DrawRectangle(float64(newWidth-20), 0, 20, float64(newHeight))
+	dc.Fill()
+	//bottom
+	dc.DrawRectangle(0, float64(newHeight-20), float64(newWidth), 20)
+	dc.Fill()
+	dc.DrawImage(blackImage, 0, 0)
+	dc.Clip()
+	resultImage = dc.Image()
+
+	outputFile, err = os.Create("output/go-red3.png")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	err = png.Encode(outputFile, resultImage)
+	if err != nil {
+		fmt.Println("Error encoding output image:", err)
+		return
+	}
+
+	result := image.NewGray(resultImage.Bounds())
+	draw.Draw(result, result.Bounds(), resultImage, resultImage.Bounds().Min, draw.Src)
+
+	for y := result.Bounds().Min.Y; y < result.Bounds().Max.Y; y++ {
+		for x := result.Bounds().Min.X; x < result.Bounds().Max.X; x++ {
+			result.At(x, y)
+			// fmt.Print(result.At(x, y))
+		}
+	}
+	fmt.Println("")
+
+	outputFile, err = os.Create("output/go-red-grey1.png")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	err = png.Encode(outputFile, result)
+	if err != nil {
+		fmt.Println("Error encoding output image:", err)
+		return
+	}
+}
+
+func createBlackImage(buffer *[]color.Gray) {
+	newWidth, newHeight := 800, 480
+	blackImage := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+	fontPath, _ := findfont.Find("arial.ttf")
+
+	dc := gg.NewContextForImage(blackImage)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	if err := dc.LoadFontFace(fontPath, 96); err != nil {
+		panic(err)
+	}
+	dc.DrawStringAnchored("On Air!!", float64(newWidth/2), float64(newHeight/2), 0.5, 0.5)
+
+	dc.SetRGB(0, 0, 0)
+	dc.DrawImage(blackImage, 0, 0)
+	dc.Clip()
+	resultImage := dc.Image()
+	result := image.NewGray(resultImage.Bounds())
+	draw.Draw(result, result.Bounds(), resultImage, resultImage.Bounds().Min, draw.Src)
+
+	var i = 0
+	for y := result.Bounds().Min.Y; y < result.Bounds().Max.Y; y++ {
+		for x := result.Bounds().Min.X; x < result.Bounds().Max.X; x++ {
+			result.At(x, y)
+			// fmt.Print(resultImage.At(x, y))
+			// (*buffer)[i] = result.At(x, y).(color.Gray)
+			*buffer = append(*buffer, result.At(x, y).(color.Gray))
+			i++
+		}
+	}
+	fmt.Println("")
+}
+
+func compressToXBM(xbm *[]uint8, buffer *[]color.Gray) {
+	for i := 0; i < len(*buffer); i++ {
+		if i%8 == 0 {
+			*xbm = append(*xbm, 0) // add a byte if needed
+		}
+		// fmt.Printf("i: %v %02b \n", i, (*buffer)[i].Y)
+		if (*buffer)[i].Y > 127 {
+			// Set the corresponding bit in the buffer
+			(*xbm)[i/8] |= 1 << uint(7-(i%8))
+		}
+	}
+}
+
+func blackLayer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Last-Modified", "*")
+	var raw []color.Gray
+	createBlackImage(&raw)
+	var xbm []uint8
+	compressToXBM(&xbm, &raw)
+	// fmt.Fprintf(w, "", raw)
+	for _, pixel := range xbm {
+		// Assuming little-endian byte order for simplicity
+		binary.Write(w, binary.LittleEndian, pixel)
+	}
+	// fmt.Println("data: %v", raw)
+}
+
+func main() {
+	createImages()
+	fmt.Println("Starting Server on 8090")
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	fs := http.FileServer(http.Dir("output"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.HandleFunc("/blackLayer", blackLayer)
 
 	http.ListenAndServe(":8090", nil)
 }
--- readme.md
+++ readme.md
@@ -6,10 +6,20 @@ go run main.go
 This builds an image and serves it via the go file server.
 
 Sample curl:
-```curl -i -H "If-Modified-Since: Sun, 15 Oct 2023 00:59:39 GMT" 'http://localhost:8090/static/go-black.bmp'```
+```curl -i -H "If-Modified-Since: Sun, 15 Oct 2023 00:59:39 GMT" 'http://192.168.3.148:8090/static/go-black.bmp'```
+```curl -i --head -H "If-Modified-Since: Sun, 15 Oct 2023 00:59:39 GMT" 'http://192.168.3.148:8090/static/go-black.bmp'```
 
 ## Issues:
-* Color depth is 24-bit, which is insane for 1 B/W images.  
+???
+
+## Status
+A bit in flux at the moment.  The esp32 didn't have the memory to decode 2 tiny/compressed pngs (one worked fine). I'm re-working to generate the image data in this server in a vaguely xbm format.  
+
+The format that the e-paper wants is an 800\*480 bit array, or a byte array of size (800*480/8) where each bit represents a pixel. 
+
+The current structure reflects my learning path and is probably inefficient.  Right now, I'm generating a full-color image, converting it to grayscale, then "compressing" into XBM.  The grayscale/contert to bit should be done at the same time.
+
+Also, these files should be written to disk so that the file server can preserve the last-modified time.
 
 ## generate images from python
 ```
